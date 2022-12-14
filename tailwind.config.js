4d18304c8718d4921adfc819d68d83b8938f494a
Lavi333 <75388274+Lavi333@users.noreply.github.com>
2022-12-14 12:23:13 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        base: '#616A7B',
+        weiss: '#F6F2E8',
+        gelb: '#D7CE81',
+        orange: '#964A33',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito'],
+        aclonica:['Aclonica'],
+        worksans:['Work+Sans'] 
+        
+      }
+      
+    },
+  },
+  variants: {},
+  plugins: [],
+}
