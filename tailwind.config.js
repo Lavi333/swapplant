5f7fe7366d7476170c1888144e271ad6d1c813bd
Lavi333 <75388274+Lavi333@users.noreply.github.com>
2023-01-26 12:04:46 +0100

    add new color in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ module.exports = {
     extend: {
       colors: {
         beige: '#F1EBDD',
+        pink:'#ECD7C8',
         gelb: '#D7CE81',
         gruen: '#36503D',
+        lightorange:'#B66F39',
         orange: '#964A33',
+        brownblack: '#3F3E3E',
+        
+
         
         secondary: {
           100: '#E2E2D5',
@@ -18,7 +23,9 @@ module.exports = {
         aclonica:['Aclonica'],
         worksans:['Work+Sans'] 
         
-      }
+      },
+
+      
       
     },
   },
